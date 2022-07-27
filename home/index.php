f967bb9acc82618dd7ceb591e0b95eb8095fe56e
Saif <Saif@SALKHIDT2658.salsoft.net>
2022-07-27 20:14:14 +0500

    Initial User

---
+++ home/index.php
@@ -0,0 +1,23 @@
+<?php include '../layout/header.php'; ?>
+
+<section class="banner-section h100vh d-flex align-items-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="bannerText text-white">
+                    <h1 class="alghato head-1">Clever Investing Make Money Grow</h1>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+                        ut labore et dolore magna aliquyam.</p>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <figure class="mb-0">
+                    <img src="../assets/images/man.png" alt="Banner Image" class="image-banner mw-100">
+                </figure>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include '../layout/footer.php'; ?>
