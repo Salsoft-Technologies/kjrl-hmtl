5d3d931de4f31bbf72fd5d5bf54ef81008232806
Saif <Saif@SALKHIDT2658.salsoft.net>
2022-07-28 19:37:32 +0500

    KJRL Changes

--- home/index.php
+++ home/index.php
@@ -1,19 +1,77 @@
 <?php include '../layout/header.php'; ?>
 
-<section class="banner-section h100vh d-flex align-items-center">
+<section class="banner-section h100vh d-flex flex-column justify-content-center">
     <div class="container">
-        <div class="row">
+        <div class="row align-items-center">
             <div class="col-md-7">
                 <div class="bannerText text-white">
                     <h1 class="alghato head-1">Clever Investing Make Money Grow</h1>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+                    <p class="py-4">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                        tempor invidunt
                         ut labore et dolore magna aliquyam.</p>
+                    <div class="themeBtn d-flex over-flow-hidden">
+                        <a href="#" class="btn-theme-primary">Let's Talk <i class="bi bi-arrow-right pl-2"></i></a>
+                    </div>
+
                 </div>
             </div>
             <div class="col-md-5">
                 <figure class="mb-0">
-                    <img src="../assets/images/man.png" alt="Banner Image" class="image-banner mw-100">
+                    <img src="../assets/images/man.png" alt="Banner Image" class="image-banner mw-100"
+                        draggable="false">
                 </figure>
+            </div>
+        </div>
+    </div>
+    <div class="statsBox bg-white py-4 h-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-3">
+                    <div class="icon-box">
+                        <div class="d-flex gap-15">
+                            <div class="icon-shadow">
+                                <figure class="mb-0">
+                                    <img src="../assets/images/planning.png" alt="Planning" class="mw-100">
+                                </figure>
+                            </div>
+                            <div class="icon-content-box">
+                                <h5 class="mb-1">Wealth Planning</h5>
+                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="icon-box">
+                        <div class="d-flex gap-15">
+                            <div class="icon-shadow">
+                                <figure class="mb-0">
+                                    <img src="../assets/images/planning.png" alt="Planning" class="mw-100">
+                                </figure>
+                            </div>
+                            <div class="icon-content-box">
+                                <h5 class="mb-1">Wealth Planning</h5>
+                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="icon-box">
+                        <div class="d-flex gap-15">
+                            <div class="icon-shadow">
+                                <figure class="mb-0">
+                                    <img src="../assets/images/planning.png" alt="Planning" class="mw-100">
+                                </figure>
+                            </div>
+                            <div class="icon-content-box">
+                                <h5 class="mb-1">Wealth Planning</h5>
+                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- layout/top-bar.php
+++ layout/top-bar.php
@@ -1,41 +1,49 @@
 <header>
     <div class="container">
-        <div class="row">
+        <div class="row align-items-center">
             <div class="col-md-2">
-                <p class="text-white">Sign in/Register</p>
+                <div class="socialIcon">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                    <a href="#"><i class="fab fa-vk"></i></a>
+                    <a href="#"><i class="fab fa-vine"></i></a>
+                </div>
             </div>
             <div class="col-md-8">
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarScroll"
-                        aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse align-items-center" id="navbarScroll">
-                        <ul class="navbar-nav ml-auto my-2 my-lg-0 navbar-nav-scroll" style="max-height: 100px;">
-                            <li class="nav-item px-3">
-                                <a class="nav-link" href="#">Pyramid</a>
-                            </li>
-                            <li class="nav-item px-3">
-                                <a class="nav-link" href="#">Subscription</a>
-                            </li>
-                        </ul>
-                        <div class="logo d-flex align-items-center">
-                            <a class="navbar-brand" href="../home/"><img src="../assets/images/logo.png" alt="Logo"
-                                    class="mw-100" draggable="false"></a>
+                <div class="mainMenu">
+                    <nav class="navbar navbar-expand-lg navbar-light p-md-0">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarScroll"
+                            aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse align-items-center" id="navbarScroll">
+                            <ul class="navbar-nav ml-auto my-2 my-lg-0 navbar-nav-scroll" style="max-height: 100px;">
+                                <li class="nav-item px-3">
+                                    <a class="nav-link" href="#">Pyramid</a>
+                                </li>
+                                <li class="nav-item px-3">
+                                    <a class="nav-link" href="#">Subscription</a>
+                                </li>
+                            </ul>
+                            <div class="logo d-flex align-items-center">
+                                <a class="navbar-brand" href="../home/"><img src="../assets/images/logo.png" alt="Logo"
+                                        class="mw-100" draggable="false"></a>
+                            </div>
+                            <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll" style="max-height: 100px;">
+                                <li class="nav-item px-3">
+                                    <a class="nav-link" href="#">About us</a>
+                                </li>
+                                <li class="nav-item px-3">
+                                    <a class="nav-link" href="#">Contact us</a>
+                                </li>
+                            </ul>
                         </div>
-                        <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll" style="max-height: 100px;">
-                            <li class="nav-item px-3">
-                                <a class="nav-link" href="#">About us</a>
-                            </li>
-                            <li class="nav-item px-3">
-                                <a class="nav-link" href="#">Contact us</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+                    </nav>
+                </div>
             </div>
             <div class="col-md-2">
-                <p class="text-white">Sign in/Register</p>
+                <p class="text-white"><a href="#" class="text-white">Sign in/Register <i class="bi bi-arrow-right pl-2"></i></a></p>
             </div>
         </div>
     </div>
