ca9eae3621f0a29d3784c6e5d6d204af1e7f4668
Saif <Saif@SALKHIDT2658.salsoft.net>
2022-08-02 20:31:32 +0500

    Add Payrimid Screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,41 @@ jQuery(window).scroll(function(event) {
         jQuery('header').addClass('position-fixed');
     }
 });
+
+
+jQuery('#addFeild').click(function(){
+    var cloneBox = jQuery(this).parents('.form-group').clone();
+      cloneBox.find('#addFeild').remove();
+      var getLenght = jQuery('.active .form-group').length;
+      if(getLenght < 3) {
+        jQuery(this).parents('.step-form').append(cloneBox);
+      }
+     
+})
+
+
+function activeTab () {
+    const list = [...document.querySelectorAll('.step-form')];
+    document.querySelector('.total-steps').innerText = list.length;
+    const widthFind = 100 / list.length;
+    document.querySelector('.activeBar').style.width = widthFind +'%';
+    const active = document.querySelector('.step-form.active');
+    const result =  list.indexOf(active) + 1;
+    document.querySelector('.active-steps').innerText = result;
+}
+
+
+
+jQuery('#develop').click(function(){
+    jQuery('.parimid-greets').hide();
+    jQuery('.pyrimid-flow').show();
+    activeTab ();
+});
+
+function procedBtn() {
+    const active = document.querySelectorAll('.step-form.active');
+    active.forEach((item) => {
+        item.classList.remove('active');
+    });
+    active.indexOf(active) -1
+}
